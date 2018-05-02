ece98a330bfc7389e98021a2dcc32d80e011cb06
abarbee129 <abarbee129@PR34.hhs.win.fuhsd.org>
2018-05-02 13:51:45 -0700

    help me

--- AnimationDemo/src/Mario.java
+++ AnimationDemo/src/Mario.java
@@ -9,7 +9,10 @@ public class Mario extends Sprite {
 
 	public static final int MARIO_WIDTH = 40;
 	public static final int MARIO_HEIGHT = 60;
-
+	private double dt = 1 / 60.0;
+	private double dx, dy;
+	private double oldDx, oldDy;
+	private double ddx, ddy;
 	
 	public Mario(PImage img, int x, int y) {
 		super(img, x, y, MARIO_WIDTH, MARIO_HEIGHT);
@@ -27,7 +30,8 @@ public class Mario extends Sprite {
 
 	public void act(ArrayList<Shape> obstacles) {
 		// FALL (and stop when a platform is hit)
-		y++;
+		x += dt * (oldDx + ((ddx/2) * dt));
+		y += dt * (oldDy + ((ddy/2) * dt));
 	}
 	
 
